40c6114144deb33d670f5df8c3802b4451dad3e9
SwayamprakashSahoo <42139077+SwayamprakashSahoo@users.noreply.github.com>
2020-08-08 16:44:59 +0530

    Add files via upload

---
+++ a1-q2.c
@@ -0,0 +1,21 @@
+//program to print the factorial of a number
+
+#include <stdio.h>
+void main()
+{
+	int n, fac = 1, i = 1;
+	do
+	{
+		printf("Please enter a number:\n");
+		scanf_s("%d", &n);
+	} while (n < 0);
+
+	while (n > 0 && i <= n)
+	{
+		fac = fac * i;
+		i++;
+	}
+
+	printf("The factorial for the number %d is: %d \n", n, fac);
+	return 0;
+}
