19a083bab9d4d03df53da696a611c3e9a55e10ce
SwayamprakashSahoo <42139077+SwayamprakashSahoo@users.noreply.github.com>
2020-08-09 17:37:15 +0530

    Create a1-q3.py

---
+++ a1-q3.py
@@ -0,0 +1,13 @@
+#program to sum the series 1 + 1/2 + 1/3 + ... till the sum doesn't change by more than .001
+denominator = 1
+sum = 0
+count=0
+print("The terms in the series include:")
+num = float(1/denominator)
+while num>0.001:
+    num = 1 / denominator
+    print("1/",(count+1))
+    sum += num
+    denominator += 1
+    count+=1
+print('The sum of the above series is .',sum)
